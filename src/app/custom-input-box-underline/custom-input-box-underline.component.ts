25bf3413e775f8b2214a290ac0787398353822f0
Dale56 <thefallout4fan@gmail.com>
2025-08-10 15:00:53 -0400

    Adjusted powers row spacing and underline element now has variable size using [width].

--- src/app/custom-input-box-underline/custom-input-box-underline.component.ts
+++ src/app/custom-input-box-underline/custom-input-box-underline.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-custom-input-box-underline',
@@ -8,4 +8,5 @@ import { Component } from '@angular/core';
 })
 
 export class CustomInputBoxUnderlineComponent {
+  @Input() width: string = "250px"; 
 }
