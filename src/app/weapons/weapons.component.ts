ac1de2b240f8d01631990c005a05065cb0f4f2e8
Dale56 <thefallout4fan@gmail.com>
2025-08-11 16:56:44 -0400

    The rows in weapons and powers can now be added and removed as needed. Divs that can have elements added so that it will go outside of its own boundaries now have overflow to make them scrollable.

--- src/app/weapons/weapons.component.ts
+++ src/app/weapons/weapons.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { WeaponRowComponent } from "../weapon-row/weapon-row.component";
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-weapons',
-  imports: [WeaponRowComponent, NgFor],
+  imports: [WeaponRowComponent, NgFor, NgIf],
   templateUrl: './weapons.component.html',
   styleUrl: './weapons.component.css'
 })
 export class WeaponsComponent {
-  weaponLines = Array(7).fill(1).map((x,i)=>i+1); 
+  weaponLines = Array(6).fill(1).map((x,i)=>i+1); 
+
+ addWeapon() {
+  this.weaponLines.push(1);
+ } 
+
+ removeWeapon() {
+  if( this.weaponLines.length > 6) {
+    this.weaponLines.pop();
+  }
+ }
+
 }
+
--- src/powers/powers.component.ts
+++ src/powers/powers.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { PowerRowComponent } from "../app/power-row/power-row.component";
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-powers',
-  imports: [PowerRowComponent, NgFor],
+  imports: [PowerRowComponent, NgFor, NgIf],
   templateUrl: './powers.component.html',
   styleUrl: './powers.component.css'
 })
 export class PowersComponent {
-  powerLines = Array(7).fill(1).map((x,i)=>i+1); 
+  powerLines = Array(6).fill(1).map((x,i)=>i+1); 
+
+  addPower() {
+    this.powerLines.push(1)
+  }
+
+  removePower() {
+    if (this.powerLines.length > 6) {
+      this.powerLines.pop();
+    }
+  }
+
 }
+
