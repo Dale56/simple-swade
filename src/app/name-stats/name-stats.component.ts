c902afa1705014c7e6441ce9432c52633ad804e7
Dale56 <thefallout4fan@gmail.com>
2025-08-12 14:13:41 -0400

    corrected wound buttons logic

--- src/app/name-stats/name-stats.component.ts
+++ src/app/name-stats/name-stats.component.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, QueryList, ViewChildren, viewChildren } from '@angular/core';
 import { CustomInputBoxComponent } from "../custom-input-box/custom-input-box.component";
 import { NgFor, NgForOf } from '@angular/common';
 import { CustomInputBoxUnderlineComponent } from "../custom-input-box-underline/custom-input-box-underline.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-name-stats',
-  imports: [CustomInputBoxComponent, NgForOf, CustomInputBoxUnderlineComponent],
+  imports: [CustomInputBoxComponent, NgForOf, FormsModule],
   templateUrl: './name-stats.component.html',
   styleUrl: './name-stats.component.css'
 })
 export class NameStatsComponent {
   statsToShow = ['Name', 'Race', 'Description', 'Bennies'];
+
+  woundsStates = [false, false, false]
+  fatigueStates = [false, false]
+
+  validateWound(num: number) {
+    for (let i = 0; i < this.woundsStates.length; i++) {
+      if (i != num) {
+        this.woundsStates[i] = false;
+      } else {
+        this.woundsStates[i] = true;
+      }
+    }
+  }
+  validateFatigue(num: number) {
+    for (let i = 0; i < this.fatigueStates.length; i++) {
+      if (i != num) {
+        this.fatigueStates[i] = false;
+      } else {
+        this.fatigueStates[i] = true;
+      }
+    }
+  }
 }
+
